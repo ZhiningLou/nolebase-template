91f907bd6e30b036b577f561ca35bc40906a1a33
Ken Lou 娄智宁 <zhininglou.lzn@gmail.com>
2025-02-25 07:43:26 +0800

    Update

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,6 +1,6 @@
 /** 文本 */
-export const siteName = 'Nólëbase'
-export const siteShortName = 'Nólëbase'
+export const siteName = 'Zhining Lou'
+export const siteShortName = 'Zhining Lou'
 export const siteDescription = '记录回忆，知识和畅想的地方'
 
 /** 文档所在目录 */
